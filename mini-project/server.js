fb6e17ab2b2e7cfde69a03f7bdcefd8bbff293a4
Devu2004 <devansh7595@gmail.com>
2025-09-26 21:14:30 +0530

    mini porject completed

--- mini-project/server.js
+++ mini-project/server.js
@@ -10,16 +10,43 @@ server.use(express.json())
 // creating empty array for pushing notes data
 let notes = []
 
-// creating post method for getting data from frontend
+// creating POST method for getting data from frontend
 server.post('/notes',(req,res)=>{
     // push data in arr
     notes.push(req.body)
     // sending response in json format
     res.json({
         message:'Notes added succefully',
-        notes:notes
+    })  
+})
+
+// now GET for sending data to frontend 
+server.get('/notes',(req,res)=>{
+    res.json(notes)
+})
+
+// now DELETE method for deletion 
+server.delete('/notes/:index',(req,res)=>{
+    // /:index colon says this is dynamic deletion from any index
+    // req.params is used thats why because it is reponsible for sending an unique data like x and other plateform 
+    const index = req.params.index;
+    delete notes[index];
+    res.json({
+        message:'Note deleted Successfully'
     })
-    
+})
+
+// now PATCH method for updating data dynamically
+server.patch('/notes/:index',(req,res)=>{
+    // this line is help to get index 
+    const index = req.params.index;
+    // this is object destructring of json to get description key
+    const {description} = req.body
+    // in this we update the description
+    notes[index].description = description
+    res.json({
+        message:'Note updated!'
+    })
 })
 
 // creating port 
@@ -27,3 +54,5 @@ server.listen(3000,()=>{
     console.log('Server is runnig on port 3000');
     
 })
+
+// i have test all of these with @postman.
