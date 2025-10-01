8bdf7ba437c4801ef2fe95206d6f7c17a84468b3
Devu2004 <devansh7595@gmail.com>
2025-10-01 20:11:10 +0530

    advance mini project done with mongosse and postman

--- advance-mini-project/server.js
+++ advance-mini-project/server.js
@@ -1,7 +1,55 @@
 const express = require('express')
 const mongoose = require('./src/db/db')
+const noteModel = require('./src/model/db.model')
 const server = express();
 
+server.use(express.json())
+
+server.post('/notes',async (req,res)=>{
+    const {title,content} = req.body
+    await noteModel.create({
+        title,content
+    })
+    res.json({
+        message:"Note created Succefully!"
+    })
+})
+
+server.get('/notes',  async (req,res)=>{
+    const notes = await noteModel.find()
+
+    res.json({
+        message:'Notes fetched Successfully!',
+        notes
+    })
+})
+
+server.delete('/notes/:id',async (req,res)=>{
+    const index = req.params.id
+
+    await noteModel.findOneAndDelete({
+        _id: index
+    })
+
+    res.json({
+        message:'Note deleted Successfully!'
+    })
+})
+
+server.patch('/notes/:id',async (req,res)=>{
+    const noteId = req.params.id
+    const {title} = req.body
+
+    await noteModel.findByIdAndUpdate({
+        _id:noteId
+    },{
+        title:title
+    })
+
+    res.json({
+        message:'Note updated succefully!'
+    })
+})
 mongoose()
 // But we call the database here
 server.listen(3000,()=>{
